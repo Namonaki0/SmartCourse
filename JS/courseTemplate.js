916417763cf5a239008da92254187ebada0e58fe
Andre Ferreira <andre.ferreira>
2021-04-02 23:45:32 +0100

    modal functionality

--- JS/courseTemplate.js
+++ JS/courseTemplate.js
@@ -1,60 +1,174 @@
+//? GLOBAL
 const imageSrc = document.querySelector(".course-img");
 const courses = document.querySelectorAll(".course-desc img");
 const courseCta = document.querySelector("#course-cta");
-const courseOutputWrapper = document.querySelector("#course-output-wrapper");
 const courseCtaAll = document.querySelectorAll(".cta-course-page");
+const courseOutputWrapper = document.querySelector("#course-output-wrapper");
+const courseTitle = courseOutputWrapper.querySelector("h4");
+const courseDetails = courseOutputWrapper.querySelector("p");
+const closeBtn = courseOutputWrapper.querySelector(".close-btn");
+const bottomDetails = document.querySelector(".bottom-details");
+const instructorDetails = bottomDetails.querySelectorAll("span");
 
-const courseImages = [
-  { src: "Images/Image1.jpeg" },
-  { src: "Images/Image2.jpg" },
-  { src: "Images/Image3.jpeg" },
-  { src: "Images/Image4.jpeg" },
-  { src: "Images/image5.jpg" },
-  { src: "Images/image6.jpeg" },
-  { src: "Images/image7.jpg" },
+console.log(courseCtaAll);
+
+//? COURSE INFORMATION
+const courseInfo = [
+  {
+    //? PATTERN DESIGN
+    name: "pattern design",
+    src: "Images/Image1.jpeg",
+    details:
+      "The most comprehensive course on how to create the best looking patterns for your web and mobile applications. Highly rated by the best in the business",
+    instructor: "Anthony Jenkins",
+    enrolled: "624 people",
+    rating: "★★★★★",
+  },
+  {
+    //? DESIGN THINKING
+    name: "Design Thinking",
+    src: "Images/Image2.jpg",
+    details:
+      "From conception to prototyping and everything else you need to know about the design for web applications. ",
+    instructor: "Mark Rogers",
+    enrolled: "522 people",
+    rating: "★★★★",
+  },
+  {
+    //? UX/UI FUNDAMENTALS
+    name: "UX/UI Fundamentals",
+    src: "Images/Image3.jpeg",
+    details:
+      "Get familiarized with all the important terminology when it comes to UX/UI and learn about key concepts such as 'user journey' and 'empathy'.",
+    instructor: "Pablo Francisco",
+    enrolled: "972 people",
+    rating: "★★★★★",
+  },
+  {
+    //? PYTHON BOOTCAMP
+    name: "Python Bootcamp",
+    src: "Images/Image4.jpeg",
+    details:
+      "One of the most interesting and diverse language you can learn. This bootcamp will teach the most important core concepts and implementations for your future projects.",
+    instructor: "Robert Stevens",
+    enrolled: "1634 people",
+    rating: "★★★★★",
+  },
+  {
+    //? COMPUTER SCIENCE
+    name: "Computer Science",
+    src: "Images/image5.jpg",
+    details:
+      "Get to know the ins and outs of your computer by diving deep into our CS course. It will provide you with invaluable knowledge.",
+    instructor: "Amelia Borges",
+    enrolled: "2652 people",
+    rating: "★★★★★",
+  },
+  {
+    //? HTML & CSS BOOTCAMP
+    name: "HTML & CSS Bootcamp",
+    src: "Images/image6.jpeg",
+    details:
+      "Go from absolute beginner to a pro by creating visually pleasing webpages in this project based course.",
+    instructor: "Kevin Powell",
+    enrolled: "10239 people",
+    rating: "★★★★★",
+  },
 ];
 
-courseCtaAll.forEach((cta) => {
-  cta.addEventListener("click", (e) => {
-    cta.href = "#course-output-wrapper";
-    courseOutputWrapper.style.display = "grid";
-    courseOutputWrapper.style.animation = ".2s courseModal";
+const modalProps = () => {
+  if (window.screen.width >= 900 && window.screen.height >= 750) {
+    courseOutputWrapper.style.display = "flex";
+    courseOutputWrapper.style.animation = "courseModal";
+    courseOutputWrapper.style.animationDuration = "150ms";
     courseOutputWrapper.style.opacity = "1";
     courseOutputWrapper.style.visibility = "visible";
     courseOutputWrapper.style.pointerEvents = "all";
-    const btnTarget = e.target;
-    if (btnTarget.classList.contains("python-bootcamp")) {
-      // console.log("CONTAINS");
-      imageSrc.src = courseImages[3].src;
-      // console.log(imageSrc.src);
-    } else {
-      imageSrc.src = courseImages[0].src;
-      // console.log("DOES NOT");
+    document.body.style.overflowY = "hidden";
+  } else {
+    courseOutputWrapper.style.display = "";
+    courseOutputWrapper.style.animation = "courseModal";
+    courseOutputWrapper.style.animationDuration = "150ms";
+    courseOutputWrapper.style.opacity = "1";
+    courseOutputWrapper.style.visibility = "visible";
+    courseOutputWrapper.style.pointerEvents = "all";
+    document.body.style.overflowY = "hidden";
+  }
+};
+
+//? CTA - MODAL DISPLAY
+courseCtaAll.forEach((cta) => {
+  cta.addEventListener("click", (e) => {
+    e.preventDefault();
+    modalProps();
+
+    const btnTargetClass = e.target.classList;
+    switch (btnTargetClass[1]) {
+      case "pattern-design":
+        imageSrc.src = courseInfo[0].src;
+        courseTitle.innerText = courseInfo[0].name;
+        courseDetails.innerText = courseInfo[0].details;
+        instructorDetails[0].innerText = courseInfo[0].instructor;
+        instructorDetails[1].innerText = courseInfo[0].enrolled;
+        instructorDetails[2].innerText = courseInfo[0].rating;
+        break;
+      case "design-thinking":
+        imageSrc.src = courseInfo[1].src;
+        courseTitle.innerText = courseInfo[1].name;
+        courseDetails.innerText = courseInfo[1].details;
+        instructorDetails[0].innerText = courseInfo[1].instructor;
+        instructorDetails[1].innerText = courseInfo[1].enrolled;
+        instructorDetails[2].innerText = courseInfo[1].rating;
+        break;
+      case "ux-ui-fundamentals":
+        imageSrc.src = courseInfo[2].src;
+        courseTitle.innerText = courseInfo[2].name;
+        courseDetails.innerText = courseInfo[2].details;
+        instructorDetails[0].innerText = courseInfo[2].instructor;
+        instructorDetails[1].innerText = courseInfo[2].enrolled;
+        instructorDetails[2].innerText = courseInfo[2].rating;
+        break;
+      case "python-bootcamp":
+        imageSrc.src = courseInfo[3].src;
+        courseTitle.innerText = courseInfo[3].name;
+        courseDetails.innerText = courseInfo[3].details;
+        instructorDetails[0].innerText = courseInfo[3].instructor;
+        instructorDetails[1].innerText = courseInfo[3].enrolled;
+        instructorDetails[2].innerText = courseInfo[3].rating;
+        break;
+      case "computer-science":
+        imageSrc.src = courseInfo[4].src;
+        courseTitle.innerText = courseInfo[4].name;
+        courseDetails.innerText = courseInfo[4].details;
+        instructorDetails[0].innerText = courseInfo[4].instructor;
+        instructorDetails[1].innerText = courseInfo[4].enrolled;
+        instructorDetails[2].innerText = courseInfo[4].rating;
+        break;
+      case "html-css-bootcamp":
+        imageSrc.src = courseInfo[5].src;
+        courseTitle.innerText = courseInfo[5].name;
+        courseDetails.innerText = courseInfo[5].details;
+        instructorDetails[0].innerText = courseInfo[5].instructor;
+        instructorDetails[1].innerText = courseInfo[5].enrolled;
+        instructorDetails[2].innerText = courseInfo[5].rating;
+        break;
+      default:
+        imageSrc.src = courseInfo[1].src;
+        break;
     }
   });
 });
 
-const fetchImage = async () => {
-  imageSrc.src = await courseImages[0].src;
-  // console.log(imageSrc);
-};
-
-fetchImage();
-
-const ctaPage = document.querySelector(".cta-course-page");
-
-ctaPage.addEventListener("click", () => {
-  courseOutputWrapper.style.display = "grid";
-  courseOutputWrapper.style.animation = ".5s courseModal";
-  courseOutputWrapper.style.opacity = "1";
-  courseOutputWrapper.style.visibility = "visible";
-  courseOutputWrapper.style.pointerEvents = "all";
-});
-
-// //? CLOSE MODAL BY CLICKING OUTSIDE
-
+//? CLOSE MODAL BY CLICKING OUTSIDE
 window.onclick = function (event) {
   if (event.target == courseOutputWrapper) {
     courseOutputWrapper.style.display = "none";
+    document.body.style.overflowY = "unset";
   }
+};
+
+//? CLOSE MODAL BY CLICKING BUTTON
+closeBtn.onclick = () => {
+  document.body.style.overflowY = "unset";
+  courseOutputWrapper.style.display = "none";
 };
--- JS/index.js
+++ JS/index.js
@@ -69,121 +69,29 @@ btnSliders.forEach((slider) => {
   });
 });
 
-//? MODAL
+//? SUBSCRIBE MODAL
 
 let subscribe = document.querySelector("#subscribe");
 let modal = document.querySelector("#modal");
 
-// //? MODAL FUNCTION
+//? SUBSCRIBE MODAL FUNCTION
 
-subscribe.onclick = function () {
-  modal.style.display = "grid";
-  modal.style.animationName = "animatetop";
-  modal.style.opacity = "1";
-  modal.style.visibility = "visible";
-  modal.style.pointerEvents = "all";
-};
+// subscribe.onclick = function () {
+//   modal.style.display = "grid";
+//   modal.style.animationName = "animatetop";
+//   modal.style.opacity = "1";
+//   modal.style.visibility = "visible";
+//   modal.style.pointerEvents = "all";
+// };
+//? CLOSE MODAL BY CLICKING OUTSIDE
 
-// //? CLOSE MODAL BY CLICKING OUTSIDE
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-// //? CLOSE MODAL BY CLICKING X
-closeModal.onclick = function () {
-  modal.style.display = "none";
-};
-
-// //* VIEWPORT
-
-let width = document.getElementById("width");
-let onresize = function () {
-  width.innerText = document.body.clientWidth;
-  width.classList.add("display-width");
-  setTimeout(() => {
-    width.classList.remove("display-width");
-  }, 2000);
-};
-window.addEventListener("resize", onresize);
-
-//? COURSE OUTPUT TEMPLATE
-
-// coursePage.addEventListener("click", (e) => );
-
-class PageLoad {
-  constructor() {
-    this.$coursePage = document.querySelectorAll(".cta-course-page");
-    this.pageTemplate = document.querySelector("#course-output");
-    this.$outputBtn = document.querySelector("#output-btn");
-
-    this.addEventListeners();
-  }
-
-  addEventListeners() {
-    window.addEventListener("DOMcontentloaded", () => {
-      // this.paths();
-      this.pageLoad(page);
-      this.handleClick(page);
-    });
-
-    this.$coursePage.forEach((page) => {
-      page.addEventListener("click", (e) => {
-        this.handleClick(page);
-        e.preventDefault();
-        console.log(e);
-      });
-    });
-  }
-
-  // //? THIS WORKS
-  // template(event) {
-  //   window.addEventListener("load", (event) => {
-  //     pageTemplate.innerHTML = `
-  //     <h1 class="page">Hello</h1>
-  //     <p>loads of content</p>
-  //     `;
-  //   });
-  // }
-
-  handleClick(page) {
-    const isPatternDesign = page.classList.contains("pattern-design");
-    const isDesignThinking = page.target.classList === "design-thinking";
-    const isUxUiFundamentals = page.target.classList === "ux-ui-fundamentals";
-    const isPythonBootcamp = page.target.classList === "python-bootcamp";
-    const isComputerScience = page.target.classList === "computer-science";
-    const isHtmlCssBootcamp = page.target.classList === "html-css-bootcamp";
-
-    console.log(isPatternDesign);
-
-    if (isPatternDesign) {
-      console.log(page);
-      // template(event);
-      console.log("hello");
-    }
-  }
-}
-
-new PageLoad();
-
-let pageTemplate = document.querySelector("#course-output");
-
-pageTemplate.addEventListener("load", pageLoad);
-
-function pageLoad(page) {
-  // window.addEventListener("load", () => {
-  //   console.log(window);
-  document.addEventListener("load", () => {
-    console.log(window);
-    this.pageTemplate.innerHTML = `
-      <h1 class="page">Hello</h1>
-      <p>loads of content</p>
-      `;
-  });
-
-  // });
-}
-
-// pageLoad();
+//? CLOSE MODAL BY CLICKING X
+// closeModal.onclick = function () {
+//   modal.style.display = "none";
+// };
