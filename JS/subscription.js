931e9f7c60e125a36149c3354e2b0ec770a5522d
Andre Ferreira <andre.ferreira>
2021-04-21 23:50:23 +0100

    refactoring

--- JS/subscription.js
+++ JS/subscription.js
@@ -3,39 +3,29 @@
 const formWrapper = document.querySelector("#mc_embed_signup");
 const subscription = document.querySelector("#subscription");
 const form = document.querySelector("form");
+const submitBtn = document.querySelector("#mc-embedded-subscribe");
 
 //? SHOW SUBSCRIPTION MODAL
 
-function showModal(e) {
-  e.preventDefault();
-  formWrapper.classList.toggle("show");
-  document.body.style.overflowY = "hidden";
-}
+form.addEventListener("submit", (e) => {
+  setTimeout(() => {
+    location.replace("index.html");
+  }, 3000);
+  console.log(e);
+})(
+  // function showModal(e) {
+  //   e.preventDefault();
+  //   formWrapper.classList.toggle("show");
+  //   document.body.style.overflowY = "hidden";
+  // }
 
-subscription.addEventListener("click", showModal);
-
-//? CLOSE MODAL BY CLICKING OUTSIDE
-
-window.onclick = function (event) {
-  console.log(event.target);
-  if (event.target == formWrapper) {
-    formWrapper.style.display = "";
-    formWrapper.classList.remove("show");
-    document.body.style.overflowY = "";
-  } else if (event.target == courseOutputWrapper) {
-    courseOutputWrapper.style.display = "none";
-    document.body.style.overflowY = "unset";
-  } else {
-    return;
+  function ($) {
+    window.fnames = new Array();
+    window.ftypes = new Array();
+    fnames[0] = "EMAIL";
+    ftypes[0] = "email";
+    fnames[1] = "FNAME";
+    ftypes[1] = "text";
   }
-};
-
-(function ($) {
-  window.fnames = new Array();
-  window.ftypes = new Array();
-  fnames[0] = "EMAIL";
-  ftypes[0] = "email";
-  fnames[1] = "FNAME";
-  ftypes[1] = "text";
-})(jQuery);
+)(jQuery);
 var $mcj = jQuery.noConflict(true);
