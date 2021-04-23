a3b81aa235e25352a0af6dd2e14a19d472fdc4da
Andre Ferreira <andre.ferreira>
2021-04-23 23:45:34 +0100

    changes

--- JS/filterCourse.js
+++ JS/filterCourse.js
@@ -1,6 +1,10 @@
 const filterCourseWrapper = document.querySelector(".filter-course-wrapper");
 const filterCourse = document.querySelector("#filter-course");
-const filterUserInput = filterCourse[".filter-user-input"];
-const courseUserBtn = filterCourse[".course-filter-btn"];
+// const filterUserInput = filterCourse[".filter-user-input"];
+// const courseUserBtn = filterCourse[".course-filter-btn"];
 
-console.log("yes", filterUserInput);
+const filterUserInput = document.querySelector(".filter-user-input");
+
+filterUserInput.addEventListener("change", (e) => {
+  console.log(e.target.value);
+});
