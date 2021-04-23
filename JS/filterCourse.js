e1463144a8e8c75e095511b2423b2c531e1702fd
Andre Ferreira <andre.ferreira>
2021-04-23 23:11:55 +0100

    extras

--- JS/filterCourse.js
+++ JS/filterCourse.js
@@ -3,4 +3,4 @@ const filterCourse = document.querySelector("#filter-course");
 const filterUserInput = filterCourse[".filter-user-input"];
 const courseUserBtn = filterCourse[".course-filter-btn"];
 
-console.log(filterUserInput);
+console.log("yes", filterUserInput);
