e4e5ae54a73dcd361528e9e2b10ab3e2ae03ba17
Andre Ferreira <andre.ferreira>
2021-04-23 23:59:17 +0100

    fixes

--- JS/filterCourse.js
+++ JS/filterCourse.js
@@ -1,8 +1,13 @@
+// const { title } = require("node:process");
+
 const filterCourseWrapper = document.querySelector(".filter-course-wrapper");
 const filterCourse = document.querySelector("#filter-course");
 // const filterUserInput = filterCourse[".filter-user-input"];
 // const courseUserBtn = filterCourse[".course-filter-btn"];
-
+const titles = document.querySelectorAll("h4");
+titles.forEach((title) => {
+  console.log(title.textContent);
+});
 const filterUserInput = document.querySelector(".filter-user-input");
 
 filterUserInput.addEventListener("change", (e) => {
